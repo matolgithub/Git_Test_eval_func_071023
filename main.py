39941143bb0a4b7852140138f0644ad1fa5c15f0
Oleg <89175955650@mail.ru>
2023-10-07 14:12:17 +0300

    Next commit to Git.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-math_status = True
-while math_status:
-    input_string = input("Please, input your mathematical expression, \nhere: ")
-    if "/0" in input_string or "/ 0" in input_string:
-        result = "No_result"
-        print(f"Wrong! Null division!\nresult = {result} \nTry again.")
-    else:
-        result = eval(input_string)
-        print(result)
-        new_expression = input("Try again? (y/n)\n")
-        if new_expression != "y":
-            math_status = False
+def math_string():
+    math_status = True
+    user = input("What is your name?\n")
+    # char_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "(", ")", "*"]
+    while math_status:
+        input_string = input(f"Dear {user}! Please, input your mathematical expression, \nhere: ")
+        if "/0" in input_string or "/ 0" in input_string:
+            result = "No_result"
+            print(f"Wrong, {user}! Null division!\nresult = {result} \nTry again.")
+        else:
+            result = eval(input_string)
+            print(result)
+            new_expression = input(f"{user}, Try again? (y/n)\n")
+            if new_expression != "y":
+                math_status = False
+                print(f"Good buy, {user}!")
+
+
+if __name__ == "__main__":
+    math_string()
